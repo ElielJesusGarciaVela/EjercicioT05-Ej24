c4f960e1818d58d1f17cfb3c005188e5f70e8553
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-10-26 18:49:30 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,35 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Este programa pinta una pirámide hecha a base de números");
+        int alturaIntroducida = 0;
+        boolean correcto = false;
+        while (!correcto) {
+            try {
+                alturaIntroducida = Integer
+                        .parseInt(System.console().readLine("Introduzca la altura de la pirámide: "));
+                correcto = true;
+            } catch (NumberFormatException ex) {
+                System.out.println("Introduzca un número por favor");
+            } catch (Exception e) {
+                System.out.println("Ha ocurrido un error inesperado");
+            }
+        }
+        int planta = 1;
+        int espaciosPorDelante = alturaIntroducida - 1;
+        int i = 0;
+        while (planta <= alturaIntroducida) {
+            for (i = 1; i <= espaciosPorDelante; i++) {
+                System.out.print(" ");
+            }
+            for (i = 1; i < planta; i++) {
+                System.out.print(i);
+            }
+            for (i =planta; i > 0; i--){
+                System.out.print(i);
+            }
+            System.out.println();
+            planta++;
+            espaciosPorDelante--;
+        }
+    }
+}
